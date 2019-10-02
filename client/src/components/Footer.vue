288c8eceec77580650f6c9107e0d032d326b25e1
Vincent Vanclef <rasmusostergaard@live.dk>
2019-10-03 01:30:32 +0200

    a bit more css stuff

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -35,15 +35,17 @@
 
     <b-container fluid class="mt-5 text-white w-90 pl-5">
       <b-row>
-        <b-col sm="12" md="6" lg="3">
+        <b-col sm="12" md="6" lg="3" class="mb-3">
           <h5 class="text-uppercase font-weight-bold">titans-league</h5>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto footer-hr" />
-          <p>
-            A gaming experience above the usual. We guarantee to deliver content
-            you have never seen before.
-            <br />Join now,
-            <router-link to="/user/register">create an account</router-link>&nbsp;today!
-          </p>
+          <hr class="mb-3 mt-0 d-inline-block mx-auto footer-hr" />
+          <div class="w-95">
+            <p>
+              A gaming experience above the usual. We guarantee to deliver content
+              you have never seen before.
+              <br />Join now,
+              <router-link to="/user/register">create an account</router-link>&nbsp;today!
+            </p>
+          </div>
           <div class="text-secondary">
             Welcome to our newest member,
             <router-link :to="'/profile/' + GetNewestUser" class="text-capitalize">{{GetNewestUser}}</router-link>!
@@ -54,9 +56,9 @@
           </div>
         </b-col>
 
-        <b-col sm="12" md="6" lg="3">
+        <b-col sm="12" md="6" lg="3" class="mb-3">
           <h5 class="text-uppercase font-weight-bold text-white">Active Users</h5>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto footer-hr" />
+          <hr class="mb-3 mt-0 d-inline-block mx-auto footer-hr" />
           <p>
             There are currently
             <b class="text-info">{{GetOnlineUsers.length + GetOnlineVisitors}}</b>
@@ -87,41 +89,45 @@
           </small>
         </b-col>
 
-        <b-col sm="12" md="6" lg="2">
+        <b-col sm="12" md="6" lg="2" class="mb-3">
           <h5 class="text-uppercase font-weight-bold text-white">Profile</h5>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto footer-hr" />
-          <p>
-            <router-link to="/user/profile">View Profile</router-link>
-          </p>
-          <p>
-            <router-link to="/user/account">Ingame Account</router-link>
-          </p>
-          <p>
-            <router-link to="/user/vote">Vote Panel</router-link>
-          </p>
-          <p>
-            <router-link to="/user/donate">Donate Panel</router-link>
-          </p>
+          <hr class="mb-3 mt-0 d-inline-block mx-auto footer-hr" />
+          <ul>
+            <li class="mb-1">
+              <router-link to="/user/profile">View Profile</router-link>
+            </li>
+            <li class="mb-1">
+              <router-link to="/user/account">Ingame Account</router-link>
+            </li>
+            <li class="mb-1">
+              <router-link to="/user/vote">Vote Panel</router-link>
+            </li>
+            <lip class="mb-1">
+              <router-link to="/user/donate">Donate Panel</router-link>
+            </lip>
+          </ul>
         </b-col>
 
-        <b-col sm="12" md="6" lg="2">
+        <b-col sm="12" md="6" lg="2" class="mb-3">
           <h5 class="text-uppercase font-weight-bold text-white">links</h5>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto footer-hr" />
-          <p>
-            <router-link to="/changelog">Changelog</router-link>
-          </p>
-          <p>
-            <router-link to="/pvpstats">PvP Statistics</router-link>
-          </p>
-          <p>
-            <router-link to="/online">Online Players</router-link>
-          </p>
-          <p>
-            <router-link to="/connect">How to Connect</router-link>
-          </p>
+          <hr class="mb-3 mt-0 d-inline-block mx-auto footer-hr" />
+          <ul>
+            <li class="mb-1">
+              <router-link to="/changelog">Changelog</router-link>
+            </li>
+            <li class="mb-1">
+              <router-link to="/pvpstats">PvP Statistics</router-link>
+            </li>
+            <li class="mb-1">
+              <router-link to="/online">Online Players</router-link>
+            </li>
+            <li class="mb-1">
+              <router-link to="/connect">How to Connect</router-link>
+            </li>
+          </ul>
         </b-col>
 
-        <b-col sm="12" md="6" lg="2">
+        <b-col sm="12" md="6" lg="2" class="mb-3">
           <h5 class="text-uppercase font-weight-bold">Contact</h5>
           <hr class="mb-4 mt-0 d-inline-block mx-auto footer-hr" />
           <p>
